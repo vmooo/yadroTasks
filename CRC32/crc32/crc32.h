05a791fe46d2fd6e4a84d1a1216cd049f4ebf7fe
Ivan Mikholap <mikholapivan575@gmail.com>
2026-04-08 22:35:57 +0300

    feat(crc32): implement crc32_table computation in compile-stage

--- CRC32/crc32/crc32.h
+++ CRC32/crc32/crc32.h
@@ -1,8 +1,30 @@
 #ifndef YADROTASKS_CRC32_H
 #define YADROTASKS_CRC32_H
 
+#include <algorithm>
+#include <array>
+#include <cstdint>
+
+
 
 class crc32 {
+
+    static constexpr uint32_t primitive_polynomial = 0xEDB88320;
+
+public:
+
+    static constexpr std::array<uint32_t, 256> crc32_table = []() {
+        std::array<uint32_t, 256> table{};
+        for (size_t i = 0; i < 256; ++i) {
+            table[i] = static_cast<uint32_t>(i);
+            for (size_t j = 0; j < 8; ++j) {
+                table[i] = (table[i] & 1) ? ((table[i] >> 1) ^ primitive_polynomial) : (table[i] >> 1);
+            }
+        }
+        return table;
+    }();
+
+    static constexpr uint32_t proccess(const char* str, uint32_t);
 };
 
 
